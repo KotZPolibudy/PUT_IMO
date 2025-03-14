caea57426630478edb0cbe1dccd0cae630dc9687
KotZPolibudy <wojtek27wojtek@gmail.com>
2025-03-14 11:15:21 +0100

    zapisywanie grafik

--- TSP_heuristic/src/main.py
+++ TSP_heuristic/src/main.py
@@ -1,3 +1,4 @@
+import os
 from utils import *
 from algo.greedy_near import greedy_nearest_neighbor
 from algo.greedy_cheap import greedy_cheapest_insertion
@@ -8,7 +9,7 @@ from algo.regret_global_balanced import two_regret_global_balanced
 from algo.our_own_genetic import genetic_2tsp
 
 
-def show_paths(data, path1, path2):
+def show_paths(data, path1, path2, save_path=None):
     x = [town.x for town in data]
     y = [town.y for town in data]
     plt.scatter(x, y)
@@ -16,7 +17,11 @@ def show_paths(data, path1, path2):
         plt.plot([data[path1[i]].x, data[path1[i + 1]].x], [data[path1[i]].y, data[path1[i + 1]].y], 'r')
     for i in range(len(path2) - 1):
         plt.plot([data[path2[i]].x, data[path2[i + 1]].x], [data[path2[i]].y, data[path2[i + 1]].y], 'b')
+
+    if save_path:
+        plt.savefig(save_path)
     plt.show()
+    plt.close()
 
 
 def use_algorithm(algorithm, data, distances):
@@ -27,17 +32,20 @@ def algorithm_loop(algorithm, data, distances, n=100):
     best_cost = float('inf')
     worst_cost = float('-inf')
     total_cost = 0
+    best_paths = None
 
     for _ in range(n):
         paths = use_algorithm(algorithm, data, distances)
         if paths is None:
-            return None, None, None
+            return None, None, None, None
         cost = summary_cost(*paths, distances)
         total_cost += cost
-        best_cost = min(best_cost, cost)
+        if cost < best_cost:
+            best_cost = cost
+            best_paths = paths
         worst_cost = max(worst_cost, cost)
 
-    return best_cost, total_cost / n, worst_cost
+    return best_cost, total_cost / n, worst_cost, best_paths
 
 
 if __name__ == '__main__':
@@ -56,6 +64,7 @@ if __name__ == '__main__':
     ]
 
     results = {}
+    os.makedirs("best_paths", exist_ok=True)
 
     for algorithm in algorithms:
         algo_name = algorithm.__name__
@@ -63,11 +72,13 @@ if __name__ == '__main__':
         for path in paths:
             data = read_data(path)
             distances = measure_distances(data)
-            best, avg, worst = algorithm_loop(algorithm, data, distances)
+            best, avg, worst, best_paths = algorithm_loop(algorithm, data, distances)
             if best is None:
                 results[algo_name].append("N/A")
             else:
                 results[algo_name].append(f"{avg:.2f} ; {best:.2f} ; {worst:.2f}")
+                save_path = f"best_paths/{algo_name}_{os.path.basename(path)}.png"
+                show_paths(data, *best_paths, save_path)
 
     # Print results in table format
     header = "Algorytm | " + " | ".join(paths)
