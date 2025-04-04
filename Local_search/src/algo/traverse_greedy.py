ec26947ccc2f8feb8ae47fe6a7c166cfbf194b34
KotZPolibudy <wojtek27wojtek@gmail.com>
2025-04-04 09:10:28 +0200

    greedy algo changes

--- Local_search/src/algo/traverse_greedy.py
+++ Local_search/src/algo/traverse_greedy.py
@@ -1,18 +1,19 @@
+# algorytm greedy ale tylko z wymianą wewnątrz cykli
+
 def compute_score_change(path, distances, i, j):
-    if abs(i - j) == 1:  # i oraz j są sąsiadami
-        old_dist = distances[path[i]][path[j]] + distances[path[j]][path[(j + 1) % len(path)]]
-        new_dist = distances[path[j]][path[i]] + distances[path[i]][path[(j + 1) % len(path)]]
-    else:
-        n = len(path)
-        prev_i, next_i = (i - 1) % n, (i + 1) % n
-        prev_j, next_j = (j - 1) % n, (j + 1) % n
+    # n = len(path)
+    # warunek stopu dla zamian pierwszy i ostatni element (to powinien być ten sam i mieć 0 dist różnicy)
+    if i == 0 or j == 0:
+        return 9999
+    prev_i, next_i = (i - 1), (i + 1)
+    prev_j, next_j = (j - 1), (j + 1)
 
-        old_dist = (distances[path[prev_i]][path[i]] + distances[path[i]][path[next_i]] +
-                    distances[path[prev_j]][path[j]] + distances[path[j]][path[next_j]])
+    old_dist = (distances[path[prev_i]][path[i]] + distances[path[i]][path[next_i]] +
+                distances[path[prev_j]][path[j]] + distances[path[j]][path[next_j]])
 
-        new_dist = (distances[path[prev_i]][path[j]] + distances[path[j]][path[next_i]] +
-                    distances[path[prev_j]][path[i]] + distances[path[i]][path[next_j]])
-
+    new_dist = (distances[path[prev_i]][path[j]] + distances[path[j]][path[next_i]] +
+                distances[path[prev_j]][path[i]] + distances[path[i]][path[next_j]])
+    # print("ij dist: ", i, j, old_dist, new_dist)
     return new_dist - old_dist
 
 
@@ -23,12 +24,12 @@ def optimize_path(path, distances):
     while improved:
         improved = False
         found_improvement = False
-        for i in range(n):
+        for i in range(1, n-1):
             for j in range(i + 1, n):
                 score_change = compute_score_change(path, distances, i, j)
-                print("score_change: ", score_change)
+                # print("score_change: ", score_change)
                 if score_change < 0:
-                    print("ij: ", i, j)
+                    # print("ij: ", i, j)
                     path[i], path[j] = path[j], path[i]
                     improved = True
                     found_improvement = True
@@ -37,17 +38,17 @@ def optimize_path(path, distances):
                 break
         count += 1
         # if count % 1000000 == 0:
-        print(f"Iteration {count}: {path}")
-        if count > 5:
-            return path
+        # print(f"Iteration {count}: {path}")
+        # if count > 5:
+        #     return path
     return path
 
 
 def traverse_greedy(starting_paths, distances, _):
     path1, path2 = starting_paths
-    print("path1")
-    path1 = optimize_path(path1, distances)
-    print("path2")
-    path2 = optimize_path(path2, distances)
-    print("greedy-done")
+    # print("path1")
+    # path1 = optimize_path(path1, distances)
+    # print("path2")
+    # path2 = optimize_path(path2, distances)
+    # print("greedy-done")
     return [path1, path2]
--- Local_search/src/algo/traverse_greedy_2.py
+++ Local_search/src/algo/traverse_greedy_2.py
@@ -1,5 +1,38 @@
+# greedy, ale z zamianą pomiędzy cyklami zamiast wewnątrz cyklu
+
+def compute_score_change(path1, path2, distances, i, j):
+    a, b = path1[i - 1], path1[i]
+    c, d = path1[i + 1], path2[j - 1]
+    e, f = path2[j], path2[j + 1]
+
+    old_distance = distances[a][b] + distances[b][c] + distances[d][e] + distances[e][f]
+    new_distance = distances[a][e] + distances[e][c] + distances[d][b] + distances[b][f]
+
+    return new_distance - old_distance
+
+
 def traverse_greedy_shuffle(starting_paths, distances, _):
     path1 = starting_paths[0]
     path2 = starting_paths[1]
-    # todo
+    n = len(path2)
+    improved = True
+    count = 0
+    while improved:
+        count += 1
+        improved = False
+        found_improvement = False
+        for i in range(1, n-2):
+            for j in range(i+1, n-1):
+                score_change = compute_score_change(path1, path2, distances, i, j)
+                if score_change < 0:
+                    path1[i], path2[j] = path2[j], path1[i]
+                    # print(i, j)
+                    improved = True
+                    found_improvement = True
+                    break
+            if found_improvement:
+                break
+        # print(f"Iteration {count}: {path1} : {path2}")
+        # if count > 5:
+        #     break
     return [path1, path2]
