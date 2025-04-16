61d1b79b14a9144a60afee364919ea83a193e6ef
KotZPolibudy <wojtek27wojtek@gmail.com>
2025-04-16 11:54:44 +0200

    lab3 - initial

---
+++ Lab3 - Local_augmented/src/main.py
@@ -0,0 +1,125 @@
+import time
+from tabulate import tabulate
+from utils import *
+# starters
+from starters.randomstart import randomstart
+# algorithms
+from algo.split_regret import split_paths_regret_TSP
+from algo.traverse_steepest_edge import traverse_steepest_edge
+
+
+def use_randomstart(distances):
+    paths = randomstart(distances, choose_starting_nodes(data, distances))
+    cost = summary_cost(*paths, distances)
+    return paths, cost
+
+
+def use_construction_algo(algo, distances, n=100):
+    best_score = float('inf')
+    worst_score = float('-inf')
+    total_score = 0
+    best_solution = None
+    total_time = 0
+    best_time = float('inf')
+    worst_time = float('-inf')
+
+    # do parallel here, please ;~;
+    for _ in range(n):
+        start_time = time.perf_counter()
+        solution = algo(distances, choose_starting_nodes(data, distances))
+        elapsed_time = time.perf_counter() - start_time
+        path1, path2 = solution
+        score = summary_cost(path1, path2, distances)
+        total_score += score
+        total_time += elapsed_time
+        if score < best_score:
+            best_score = score
+            best_solution = solution
+        worst_score = max(worst_score, score)
+        best_time = min(best_time, elapsed_time)
+        worst_time = max(worst_time, elapsed_time)
+
+    return best_score, total_score / n, worst_score, best_solution, best_time, total_time / n, worst_time
+
+
+def use_local_algo(algo, distances, n=100):
+    best_score = float('inf')
+    worst_score = float('-inf')
+    total_score = 0
+    best_solution = None
+    total_time = 0
+    best_time = float('inf')
+    worst_time = float('-inf')
+    total_diff = 0
+    best_diff = 0
+
+    # do parallel here, please ;~;
+    for _ in range(n):
+        start_time = time.perf_counter()
+        start, starting_score = use_randomstart(distances)
+        solution = algo(start, distances)
+        elapsed_time = time.perf_counter() - start_time
+        path1, path2 = solution
+        score = summary_cost(path1, path2, distances)
+        diff = starting_score - score
+        total_score += score
+        total_time += elapsed_time
+        total_diff += diff
+        if score < best_score:
+            best_score = score
+            best_solution = solution
+        worst_score = max(worst_score, score)
+        best_time = min(best_time, elapsed_time)
+        worst_time = max(worst_time, elapsed_time)
+        best_diff = max(best_diff, diff)
+
+    return best_score, total_score / n, worst_score, best_solution, best_time, total_time / n, worst_time, best_diff, total_diff / n
+
+
+if __name__ == '__main__':
+    instances = [
+        '../data/kroA200.tsp',
+        '../data/kroB200.tsp'
+        ]
+    construction_algorithms = [
+        split_paths_regret_TSP,
+    ]
+    local_algorithms = [
+        traverse_steepest_edge,
+    ]
+
+    results = []
+    os.makedirs("../best_paths", exist_ok=True)
+    for insta in instances:
+        i = insta.split('/')[-1].split('.')[0]
+        os.makedirs(f"../best_paths/{i}", exist_ok=True)
+
+    for insta in instances:
+        data = read_data(insta)
+        distances = measure_distances(data)
+        i = insta.split('/')[-1].split('.')[0]
+        print(f"=============================={insta}==============================")
+
+        for algorithm in construction_algorithms:
+            algo_name = algorithm.__name__
+            print(f"==============={algo_name}===============")
+            found_best, found_avg, found_worst, found_best_paths, bt, avgt, wt = use_construction_algo(algorithm, distances)
+
+            results.append(
+                [insta, algo_name, found_best, found_avg, found_worst, bt, avgt, wt, None, None])
+            save_path = f"../best_paths/{i}/{os.path.basename(algo_name.__name__)}.png"
+            show_paths(data, *found_best_paths, save_path)
+
+        for algorithm in local_algorithms:
+            algo_name = algorithm.__name__
+            print(f"==============={algo_name}===============")
+            found_best, found_avg, found_worst, found_best_paths, bt, avgt, wt, diff_best, diff_avg = use_local_algo(
+                algorithm, distances)
+
+            results.append(
+                [insta, algo_name, found_best, found_avg, found_worst, bt, avgt, wt, diff_best, diff_avg])
+            save_path = f"../best_paths/{i}/{os.path.basename(algo_name.__name__)}.png"
+            show_paths(data, *found_best_paths, save_path)
+
+    headers = ["Instance", "Algorytm", "Best", "Avg", "Worst", "Best Time", "Avg Time", "Worst Time", "Best Diff", "Avg Diff"]
+    print(tabulate(results, headers=headers, tablefmt="grid"))
---
+++ Lab3 - Local_augmented/src/utils.py
@@ -0,0 +1,69 @@
+import os
+import random
+import matplotlib.pyplot as plt
+
+
+class Town:
+    def __init__(self, id, x, y):
+        self.id = id
+        self.x = x
+        self.y = y
+
+    def distance_to(self, town):
+        return ((self.x - town.x) ** 2 + (self.y - town.y) ** 2) ** 0.5
+
+
+def read_data(path):
+    with open(path, 'r') as file:
+        data = file.readlines()
+        data = [x.strip() for x in data]
+        data = data[6:-1]
+        data = [x.split() for x in data]
+        data = [Town(id=int(x[0]), x=int(x[1]), y=int(x[2])) for x in data]
+    return data
+
+
+def path_cost(path, distances):
+    return sum(distances[path[i]][path[i + 1]] for i in range(len(path) - 1))
+
+
+def summary_cost(path1, path2, distances):
+    return path_cost(path1, distances) + path_cost(path2, distances)
+
+
+def measure_distances(data):
+    n = len(data)
+    distances = [[0 for _ in range(n)] for _ in range(n)]
+    for i in range(n):
+        for j in range(i + 1, n):
+            distances[i][j] = round(data[i].distance_to(data[j]))
+            distances[j][i] = distances[i][j]
+    return distances
+
+
+def choose_starting_nodes(data, distances):
+    n = len(data)
+    first = random.randint(0, n - 1)
+    second = max(range(n), key=lambda x: distances[first][x])
+    return first, second
+
+
+def show_paths(data, path1, path2, save_path=None, show=False):
+    x = [town.x for town in data]
+    y = [town.y for town in data]
+    plt.scatter(x, y)
+    for i in range(len(path1) - 1):
+        plt.plot([data[path1[i]].x, data[path1[i + 1]].x], [data[path1[i]].y, data[path1[i + 1]].y], 'r')
+    for i in range(len(path2) - 1):
+        plt.plot([data[path2[i]].x, data[path2[i + 1]].x], [data[path2[i]].y, data[path2[i + 1]].y], 'b')
+
+    if save_path:
+        dir_name = os.path.dirname(save_path)
+        if dir_name and not os.path.exists(dir_name):
+            os.mkdir(save_path)
+        plt.savefig(save_path)
+
+    if show:
+        plt.show()
+
+    plt.close()
